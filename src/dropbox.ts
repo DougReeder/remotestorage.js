a12247f2578102535c3cf2514d4df62b9a2b22e4
P. Douglas Reeder <reeder.29@gmail.com>
2022-10-08 22:44:58 -0400

    Build w/ fix for case-insensitivity

--- src/dropbox.ts
+++ src/dropbox.ts
@@ -309,7 +309,7 @@ class Dropbox extends RemoteBase implements Remote {
 
       const listing = body.entries.reduce((map, item) => {
         const isDir = item['.tag'] === 'folder';
-        const itemName = item.path_lower.split('/').slice(-1)[0] + (isDir ? '/' : '');
+        const itemName = item.path_display.split('/').slice(-1)[0] + (isDir ? '/' : '');
         if (isDir){
           map[itemName] = { ETag: revCache.get(path+itemName) };
         } else {
@@ -814,7 +814,7 @@ class Dropbox extends RemoteBase implements Remote {
         }
 
         responseBody.entries.forEach(entry => {
-          const path = entry.path_lower.slice(PATH_PREFIX.length);
+          const path = entry.path_display.slice(PATH_PREFIX.length);
 
           if (entry['.tag'] === 'deleted') {
             // there's no way to know whether the entry was a file or a folder
